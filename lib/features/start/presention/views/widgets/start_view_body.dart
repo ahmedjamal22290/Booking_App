9557079fb66dc709eb86c83d5ed8d22756d72c66
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-05-30 20:16:19 +0300

    create second view

--- lib/features/start/presention/views/widgets/start_view_body.dart
+++ lib/features/start/presention/views/widgets/start_view_body.dart
@@ -6,26 +6,12 @@ class StartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FirstView();
+    return SecondView();
   }
 }
 
-class FirstView extends StatefulWidget {
+class FirstView extends StatelessWidget {
   const FirstView({super.key});
-
-  @override
-  State<FirstView> createState() => _FirstViewState();
-}
-
-class _FirstViewState extends State<FirstView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,12 +39,117 @@ class _FirstViewState extends State<FirstView>
             ],
           ),
         ),
-        const SizedBox(height: 31.03),
+        const SizedBox(height: 82.03),
         Padding(
             padding: EdgeInsets.only(
                 left: 0.09790 * MediaQuery.of(context).size.width,
                 right: 0.09870 * MediaQuery.of(context).size.width),
-            child: Column(
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Travel safely, comfortably & easily',
+                  style: Styles.headlines,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ))
+      ],
+    );
+  }
+}
+
+class SecondView extends StatefulWidget {
+  const SecondView({super.key});
+
+  @override
+  State<SecondView> createState() => _SecondViewState();
+}
+
+class _SecondViewState extends State<SecondView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> AnimationPos1;
+  late Animation<Offset> AnimationPos2;
+  late Animation<Offset> AnimationPos3;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+          height: 0.48116 * MediaQuery.of(context).size.height,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Positioned(
+                top: 0.18655 * MediaQuery.of(context).size.height,
+                right: 0.6422 * MediaQuery.of(context).size.width,
+                child: SizedBox(
+                  height: 0.20000 * MediaQuery.of(context).size.height,
+                  width: 0.3577 * MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'assets/images/start_images/Group 10.png',
+                    height: 69.1,
+                    width: 69.1,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0.0860 * MediaQuery.of(context).size.height,
+                right: 0.41496 * MediaQuery.of(context).size.width,
+                child: SizedBox(
+                  height: 0.20000 * MediaQuery.of(context).size.height,
+                  width: 0.3577 * MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'assets/images/start_images/Mask group(5).png',
+                    height: 69.1,
+                    width: 69.1,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0.14085 * MediaQuery.of(context).size.height,
+                right: 0.09327 * MediaQuery.of(context).size.width,
+                child: SizedBox(
+                  height: 0.20000 * MediaQuery.of(context).size.height,
+                  width: 0.3577 * MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'assets/images/start_images/Mask group(6).png',
+                    height: 69.1,
+                    width: 69.1,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 0.20504 * MediaQuery.of(context).size.height),
+                child: SizedBox(
+                  height: 0.20000 * MediaQuery.of(context).size.height,
+                  width: 0.3577 * MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    'assets/images/start_images/Group 9.png',
+                    height: 69.1,
+                    width: 69.1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 82.03),
+        Padding(
+            padding: EdgeInsets.only(
+                left: 0.09790 * MediaQuery.of(context).size.width,
+                right: 0.09870 * MediaQuery.of(context).size.width),
+            child: const Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
