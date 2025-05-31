047febd4e5b8766dca75ed5cf8f8abc9b0e2d5f8
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-05-31 16:30:35 +0300

    create getx controller to controlle to animate the button

---
+++ lib/features/start/controller/start_controller.dart
@@ -0,0 +1,13 @@
+import 'dart:developer';
+
+import 'package:get/get.dart';
+
+class StartController extends GetxController {
+  int page = 0;
+  void animate() {
+    // if (page == 2) {
+    // log(page.toString());
+    update();
+    // }
+  }
+}
--- lib/features/start/presention/views/widgets/start_view_body.dart
+++ lib/features/start/presention/views/widgets/start_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:booking_app/core/utils/constants.dart';
 import 'package:booking_app/core/utils/styles.dart';
+import 'package:booking_app/features/start/controller/start_controller.dart';
 import 'package:booking_app/features/start/presention/views/widgets/first_view.dart';
 import 'package:booking_app/features/start/presention/views/widgets/second_view.dart';
 import 'package:booking_app/features/start/presention/views/widgets/thired_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class StartViewBody extends StatefulWidget {
@@ -18,34 +20,79 @@ class _StartViewBodyState extends State<StartViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: 0.7958453 * MediaQuery.of(context).size.height,
-          child: PageView(
-            physics: const BouncingScrollPhysics(),
-            controller: _controller,
-            children: const [
-              FirstView(),
-              SecondView(),
-              ThiredView(),
+    return GetBuilder<StartController>(
+        init: StartController(),
+        builder: (startController) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 0.7958453 * MediaQuery.of(context).size.height,
+                child: PageView(
+                  physics: const BouncingScrollPhysics(),
+                  controller: _controller,
+                  onPageChanged: (value) {
+                    startController.page = value;
+                    startController.animate();
+                  },
+                  children: const [
+                    FirstView(),
+                    SecondView(),
+                    ThiredView(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              SmoothPageIndicator(
+                controller: _controller,
+                count: 3,
+                effect: const ExpandingDotsEffect(
+                  dotWidth: 6.72,
+                  expansionFactor: 5.56994,
+                  dotHeight: 4.8,
+                  activeDotColor: Constants.buttonsMainColor,
+                ),
+              ),
+              const Spacer(),
+              const CustomAnimatedButton(),
+              const SizedBox(height: 24),
             ],
-          ),
-        ),
-        const SizedBox(height: 15),
-        SmoothPageIndicator(
-          controller: _controller,
-          count: 3,
-          effect: const ExpandingDotsEffect(
-            dotWidth: 6.72,
-            expansionFactor: 5.56994,
-            dotHeight: 4.8,
-            activeDotColor: Constants.buttonsMainColor,
-          ),
-        ),
-        const Spacer(),
-        Container(
+          );
+        });
+  }
+}
+
+class CustomAnimatedButton extends StatefulWidget {
+  const CustomAnimatedButton({
+    super.key,
+  });
+  @override
+  State<CustomAnimatedButton> createState() => _CustomAnimatedButtonState();
+}
+
+class _CustomAnimatedButtonState extends State<CustomAnimatedButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> widthAnimation;
+  late Animation<double> iconAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 350));
+    // widthAnimation = Tween<double>(begin: ,end: 0.91608).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<StartController>(
+      builder: (controller) {
+        if (controller.page != 2 && _controller.status.isCompleted) {
+          _controller.reverse();
+        } else if (controller.page == 2 && _controller.status.isDismissed) {
+          _controller.forward();
+        }
+        return Container(
           width: 0.91608 * MediaQuery.of(context).size.width,
           height: 0.05484 * MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
@@ -58,9 +105,8 @@ class _StartViewBodyState extends State<StartViewBody> {
               style: Styles.buttonText1,
             ),
           ),
-        ),
-        const SizedBox(height: 24),
-      ],
+        );
+      },
     );
   }
 }
