82165e5d888530dfa7c4ef1ce1dca9ab5e0afac3
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-01 16:31:09 +0300

    refactor

---
+++ lib/features/home/presentation/views/widgets/home_widgets/custom_background_image.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomBackgroundImage extends StatelessWidget {
+  const CustomBackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Opacity(
+        opacity: 0.75,
+        child: Image.asset(
+          'assets/images/home_view_images/Rectangle 13.jpg',
+          width: MediaQuery.sizeOf(context).width,
+          height: 0.409670 * MediaQuery.sizeOf(context).height,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/home_widgets/custom_main_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomMainContainer extends StatelessWidget {
+  const CustomMainContainer({
+    super.key,
+    required this.child,
+  });
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height:
+            (404.57 / 594.99).toDouble() * MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(18.66),
+              topRight: Radius.circular(18.66)),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
