fd539e571bbd45bb93695d89d2d1eae6ca765837
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-10 13:22:11 +0300

    refactor

---
+++ lib/features/home/presentation/views/widgets/details_widgets/details_image_section.dart
@@ -0,0 +1,66 @@
+import 'package:booking_app/features/home/presentation/views/widgets/details_widgets/details_custom_app_bar.dart';
+import 'package:booking_app/features/home/presentation/views/widgets/details_widgets/image_details_widget.dart';
+import 'package:booking_app/features/home/presentation/views/widgets/details_widgets/next_image_widget.dart';
+import 'package:flutter/material.dart';
+
+class DetailsImageSection extends StatelessWidget {
+  const DetailsImageSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: (305.48 / 594.99).toDouble() * MediaQuery.sizeOf(context).height,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(18),
+        bottomRight: Radius.circular(18),
+      )),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(18),
+          bottomRight: Radius.circular(18),
+        ),
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/home_view_images/test hottel image.jpg',
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.4),
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+            ),
+            const Positioned(
+              top: 47.98,
+              right: 13.52,
+              left: 13.52,
+              child: DetailsCustomAppBar(),
+            ),
+            const Positioned(
+              bottom: 16.31,
+              left: 13.52,
+              child: ImageDetailsWidget(),
+            ),
+            const Positioned(
+              bottom: 16.31,
+              right: 13.52,
+              child: NextImageWIdget(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
