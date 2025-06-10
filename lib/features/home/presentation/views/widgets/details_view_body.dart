ff36c4dae0ca27ab45fe4818a66eab859f580763
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-10 13:59:13 +0300

    create service item widget

--- lib/features/home/presentation/views/widgets/details_view_body.dart
+++ lib/features/home/presentation/views/widgets/details_view_body.dart
@@ -1,13 +1,15 @@
+import 'package:booking_app/core/utils/styles.dart';
 import 'package:booking_app/features/home/presentation/views/widgets/details_widgets/about_section.dart';
 import 'package:booking_app/features/home/presentation/views/widgets/details_widgets/details_image_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       physics: BouncingScrollPhysics(),
       slivers: [
         SliverToBoxAdapter(
@@ -21,7 +23,57 @@ class DetailsViewBody extends StatelessWidget {
         SliverToBoxAdapter(
           child: AboutSection(),
         ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 13.52),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Services & Facilities',
+                  style: Styles.detailsMainTitle.copyWith(
+                    color: const Color(0xff323232),
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 11),
+                ServiceItemWidget()
+              ],
+            ),
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class ServiceItemWidget extends StatelessWidget {
+  const ServiceItemWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 180.61,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/icons/home_view_icons/check_box_icon.svg',
+            height: 18.39,
+          ),
+          const SizedBox(width: 5),
+          Text(
+            'Swimming pool',
+            textWidthBasis: TextWidthBasis.parent,
+            overflow: TextOverflow.fade,
+            style: Styles.description1.copyWith(
+              color: const Color(0xff787878),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
