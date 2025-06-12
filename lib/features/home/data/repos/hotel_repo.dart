36b4269f1eff74b79d911f38880a01afc2c52f72
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-12 14:37:33 +0300

    create failures class

--- lib/features/home/data/repos/hotel_repo.dart
+++ lib/features/home/data/repos/hotel_repo.dart
@@ -1,1 +1,6 @@
-abstract class HotelRepo {}
+import 'package:booking_app/core/data/models/hotel_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HotelRepo {
+  Future<Either<,List<HotelModel>>> fetchNearHotels();
+}
