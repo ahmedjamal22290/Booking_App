1de06c9b4c1accf96ea978b669ed16e2e4365763
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-12 15:30:31 +0300

    create hotel controller and add fetch nearby method

--- lib/features/start/controller/hotel_controller.dart
+++ lib/features/start/controller/hotel_controller.dart
@@ -1,1 +1,36 @@
+import 'package:booking_app/core/data/models/hotel_model.dart';
 
+import 'package:booking_app/features/home/data/repos/hotel_repo.dart';
+import 'package:get/get.dart';
+
+class HotelController extends GetxController {
+  var nearHotels = <HotelModel>[].obs;
+  var exploreHotels = <HotelModel>[].obs;
+  var searchResult = <HotelModel>[].obs;
+
+  final HotelRepo hotelRepo;
+
+  var isLoadingNearby = false.obs;
+  var isLoadingExplore = false.obs;
+  var isLoadingSearch = false.obs;
+
+  var nearbyErrorMessage = ''.obs;
+  var exploreErrorMessage = ''.obs;
+  var searchErrorMessage = ''.obs;
+
+  HotelController({required this.hotelRepo});
+
+  Future<void> fetchNearbyHotels(String city) async {
+    isLoadingNearby.value = true;
+    var result = await hotelRepo.fetchNearHotels(city);
+    result.fold(
+      (failure) {
+        nearbyErrorMessage.value = failure.errorMessage;
+      },
+      (list) {
+        nearHotels.assignAll(list);
+      },
+    );
+    isLoadingNearby.value = false;
+  }
+}
