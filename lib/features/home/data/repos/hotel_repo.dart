5fc0a95b23c7a85978f7136e23e8f6760a80f8be
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-12 15:29:51 +0300

    create hotel repo implemention

--- lib/features/home/data/repos/hotel_repo.dart
+++ lib/features/home/data/repos/hotel_repo.dart
@@ -1,6 +1,9 @@
 import 'package:booking_app/core/data/models/hotel_model.dart';
+import 'package:booking_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HotelRepo {
-  Future<Either<,List<HotelModel>>> fetchNearHotels();
+  Future<Either<Failures, List<HotelModel>>> fetchNearHotels(String city);
+  Future<Either<Failures, List<HotelModel>>> exploreHotels();
+  Future<Either<Failures, List<HotelModel>>> searchHotels();
 }
