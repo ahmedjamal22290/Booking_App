306efc2a8f809be97c3c1da525d764c76e80216a
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-15 15:19:05 +0300

    handle view all button in explore section

--- lib/features/home/presentation/views/widgets/home_widgets/builders/explore_hotel_builder.dart
+++ lib/features/home/presentation/views/widgets/home_widgets/builders/explore_hotel_builder.dart
@@ -1,35 +1,24 @@
+import 'package:booking_app/core/data/models/hotel_model.dart';
 import 'package:booking_app/features/home/controllers/hotel_controller.dart';
 import 'package:booking_app/features/home/presentation/views/widgets/home_widgets/explore_item.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ExploreHotelsBuilder extends StatelessWidget {
   const ExploreHotelsBuilder({
     super.key,
+    required this.exploreHotels,
   });
-
+  final List<HotelModel> exploreHotels;
   @override
   Widget build(BuildContext context) {
-    return GetX<HotelController>(builder: (controller) {
-      if (controller.isLoadingExplore.value) {
-        return const Center(
-          child: CircularProgressIndicator(),
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: exploreHotels.length,
+      itemBuilder: (context, index) {
+        return ExploreItem(
+          hotelModel: exploreHotels[index],
         );
-      }
-      if (controller.exploreErrorMessage.isNotEmpty) {
-        return Center(
-          child: Text(controller.exploreErrorMessage.value),
-        );
-      }
-      return ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: controller.exploreHotels.length,
-        itemBuilder: (context, index) {
-          return ExploreItem(
-            hotelModel: controller.exploreHotels[index],
-          );
-        },
-      );
-    });
+      },
+    );
   }
 }
--- lib/features/home/presentation/views/widgets/home_widgets/sections/explore_hotels_section.dart
+++ lib/features/home/presentation/views/widgets/home_widgets/sections/explore_hotels_section.dart
@@ -1,7 +1,10 @@
+import 'package:booking_app/core/utils/app_routs.dart';
 import 'package:booking_app/core/utils/styles.dart';
+import 'package:booking_app/features/home/controllers/hotel_controller.dart';
 import 'package:booking_app/features/home/presentation/views/widgets/home_widgets/builders/explore_hotel_builder.dart';
 import 'package:booking_app/features/home/presentation/views/widgets/home_widgets/view_all_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ExploreHotelsSection extends StatelessWidget {
   const ExploreHotelsSection({
@@ -10,33 +13,52 @@ class ExploreHotelsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Text(
-              'Explore Hotels',
-              style: Styles.mainWidgetsText1.copyWith(
-                color: Colors.black,
-                fontSize: 21.66,
+    return GetX<HotelController>(builder: (controller) {
+      if (controller.isLoadingExplore.value) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      if (controller.exploreErrorMessage.isNotEmpty) {
+        return Center(
+          child: Text(controller.exploreErrorMessage.value),
+        );
+      }
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                'Explore Hotels',
+                style: Styles.mainWidgetsText1.copyWith(
+                  color: Colors.black,
+                  fontSize: 21.66,
+                ),
               ),
+              const Spacer(),
+              ViewAllButton(
+                onTap: () {
+                  Get.toNamed(AppRouts.viewAllView,
+                      arguments: controller.exploreHotels);
+                },
+              ),
+              const SizedBox(
+                width: 12.31,
+              )
+            ],
+          ),
+          const SizedBox(height: 9.72),
+          SizedBox(
+            height: (125.26 / 594.99).toDouble() *
+                MediaQuery.sizeOf(context).height,
+            width: MediaQuery.sizeOf(context).width,
+            child: ExploreHotelsBuilder(
+              exploreHotels: controller.exploreHotels,
             ),
-            const Spacer(),
-            const ViewAllButton(),
-            const SizedBox(
-              width: 12.31,
-            )
-          ],
-        ),
-        const SizedBox(height: 9.72),
-        SizedBox(
-          height:
-              (125.26 / 594.99).toDouble() * MediaQuery.sizeOf(context).height,
-          width: MediaQuery.sizeOf(context).width,
-          child: const ExploreHotelsBuilder(),
-        ),
-      ],
-    );
+          ),
+        ],
+      );
+    });
   }
 }
