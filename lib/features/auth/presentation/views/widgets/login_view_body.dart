8703a3635328dc516003be62fb7e6c6aef637e60
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-16 14:14:14 +0300

    create email password text fields

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:booking_app/core/utils/constants.dart';
+import 'package:booking_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -5,10 +7,74 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(child: Text('Login now')),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(
+        top: MediaQuery.sizeOf(context).height * 0.23,
+        right: 14,
+        left: 14,
+      ),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Login now',
+              style: Styles.headlines1.copyWith(
+                color: const Color(0xFF1C4BC3),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 120,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Constants.buttonsMainColor),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter your email";
+              }
+
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Constants.buttonsMainColor),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter your password";
+              }
+              if (value.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+
+              return null;
+            },
+          ),
+        ],
+      ),
     );
   }
 }
