c1767cc2d3706635887c7321d2f3179c60dd3c9d
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-20 16:24:25 +0300

    create add watch list method to upload to supabase

--- lib/features/watchlist/controller/watchlist_controller.dart
+++ lib/features/watchlist/controller/watchlist_controller.dart
@@ -1,8 +1,24 @@
 import 'package:booking_app/core/data/models/hotel_model.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class WatchlistController extends GetxController {
   var watchList = <HotelModel>[].obs;
+
+  final supabase = Supabase.instance.client;
+  Future<void> addToWatchList(HotelModel hotel) async {
+    final user = supabase.auth.currentSession;
+    if (user == null) {
+      throw Exception('User not logged in');
+    }
+
+    final data = hotel.toJson(user.user.id);
+    final response = await supabase.from('watchlist').insert(data);
+    if (response.error != null) {
+      throw Exception('Failed to add to watchlist: ${response.error!.message}');
+    }
+  }
+
   void add(HotelModel hotelModel) {
     watchList.add(hotelModel);
   }
