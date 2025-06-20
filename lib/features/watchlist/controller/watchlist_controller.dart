fdaf89024c7e2a761736f6af432bf99e615d7303
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-20 16:33:43 +0300

    add fetch method

--- lib/features/watchlist/controller/watchlist_controller.dart
+++ lib/features/watchlist/controller/watchlist_controller.dart
@@ -19,6 +19,20 @@ class WatchlistController extends GetxController {
     }
   }
 
+  Future<void> getUserWatchlist() async {
+    final user = supabase.auth.currentSession;
+    if (user == null) {
+      throw Exception('User not logged in');
+    }
+    final response =
+        await supabase.from('watchlist').select().eq('user_id', user.user.id);
+    List<HotelModel> hotels = [];
+    for (var element in response) {
+      hotels.add(HotelModel.fromJson(element));
+    }
+    watchList.assignAll(hotels);
+  }
+
   void add(HotelModel hotelModel) {
     watchList.add(hotelModel);
   }
