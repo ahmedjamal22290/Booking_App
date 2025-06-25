4c3d4bf527537f260465974c5026f5c1946ce1be
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-25 18:26:02 +0300

    theme mode animation logic done

--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -12,11 +12,7 @@ class ProfileViewBody extends StatelessWidget {
         Row(
           children: [
             //change theme option
-            Container(
-              height: 50,
-              alignment: Alignment.topLeft,
-              child: Lottie.asset('assets/lottie/change_mode_animation.json'),
-            ),
+            ThemeModeAnimation(),
             Spacer(),
             SizedBox(
               height: 80,
@@ -43,5 +39,73 @@ class ProfileViewBody extends StatelessWidget {
         Text('userEmail', style: Theme.of(context).textTheme.bodyMedium),
       ],
     );
+  }
+}
+
+class ThemeModeAnimation extends StatefulWidget {
+  const ThemeModeAnimation({super.key});
+
+  @override
+  State<ThemeModeAnimation> createState() => _ThemeModeAnimationState();
+}
+
+class _ThemeModeAnimationState extends State<ThemeModeAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  LottieComposition? _composition;
+  double _startFrame = 0;
+  double _endFrame = 290;
+  bool isDark = false;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    _loadLottie();
+  }
+
+  void _loadLottie() async {
+    final compostion = await LottieComposition.fromByteData(
+      await DefaultAssetBundle.of(
+        context,
+      ).load('assets/lottie/change_mode_animation.json'),
+    );
+    setState(() {
+      _composition = compostion;
+      _controller.duration = Duration(seconds: 4);
+    });
+  }
+
+  void _onTap() async {
+    if (_composition == null) return;
+
+    double totalFrames = _composition!.endFrame - _composition!.startFrame;
+    double start = _startFrame / totalFrames;
+    double end = _endFrame / totalFrames;
+    if (!isDark) {
+      await _controller.animateTo(end);
+    } else {
+      await _controller.animateBack(start, curve: Curves.easeOutCubic);
+    }
+    setState(() {
+      isDark = !isDark;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        height: 50,
+        alignment: Alignment.topLeft,
+        child: Lottie(composition: _composition, controller: _controller),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
