4ce05ebdb330a581bffab56149c94a1bc8bd4ff1
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-06-26 15:28:27 +0300

    refactor

---
+++ lib/features/profile/presentation/views/widgets/animated_logout_button.dart
@@ -0,0 +1,26 @@
+import 'package:booking_app/core/utils/app_routs.dart';
+import 'package:booking_app/features/auth/controller/auth_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+class AnimatedLogoutButton extends StatelessWidget {
+  AnimatedLogoutButton({super.key});
+  final AuthController authController = Get.find<AuthController>();
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        authController.logout();
+        Get.offAllNamed(AppRouts.startView);
+      },
+      child: SizedBox(
+        height: 80,
+        child: Lottie.asset(
+          'assets/lottie/logout_animation.json',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:booking_app/core/utils/app_routs.dart';
-import 'package:booking_app/features/auth/controller/auth_controller.dart';
+import 'package:booking_app/features/profile/presentation/views/widgets/animated_logout_button.dart';
 import 'package:booking_app/features/profile/presentation/views/widgets/theme_mode_animation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:lottie/lottie.dart';
 
 class ProfileViewBody extends StatelessWidget {
@@ -37,27 +34,6 @@ class ProfileViewBody extends StatelessWidget {
         const SizedBox(height: 5),
         Text('userEmail', style: Theme.of(context).textTheme.bodyMedium),
       ],
-    );
-  }
-}
-
-class AnimatedLogoutButton extends StatelessWidget {
-  AnimatedLogoutButton({super.key});
-  final AuthController authController = Get.find<AuthController>();
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        authController.logout();
-        Get.offAllNamed(AppRouts.startView);
-      },
-      child: SizedBox(
-        height: 80,
-        child: Lottie.asset(
-          'assets/lottie/logout_animation.json',
-          fit: BoxFit.cover,
-        ),
-      ),
     );
   }
 }
